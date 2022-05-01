8f298ba71e6aca8d4d3ad03ac595429fb0a465a6
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-01 19:20:26 +0300

    Add files via upload

---
+++ sudoku/app.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    boardInit();
+};
+
+function boardInit() {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            let square = document.createElement('div');
+            square.id = row.toString() + '-' + col.toString();
+            square.classList.add('squares');
+            document.getElementById('board').appendChild(square);
+        }
+    }
+}
