79da71f3bce4ab829f6a623c452a2edbb8c2595a
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-01 19:20:47 +0300

    Update app.js

--- sudoku/app.js
+++ sudoku/app.js
@@ -3,5 +3,9 @@ window.onload = function () {
 };
 
 function boardInit() {
-   
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+           
+        }
+    }
 }
