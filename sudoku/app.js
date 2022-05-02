cf6081f050207a9d4ec49f8c452d53bf65a6dbe3
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-02 12:37:59 +0300

    Update app.js

--- sudoku/app.js
+++ sudoku/app.js
@@ -1,14 +1,11 @@
-window.onload = function () {
-    boardInit();
-};
-
-function boardInit() {
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            let square = document.createElement('div');
-            square.id = row.toString() + '-' + col.toString();
-            square.classList.add('squares');
-            document.getElementById('board').appendChild(square);
-        }
-    }
-}
+const puzzle = [
+    [0, 8, 0, 1, 0, 0, 0, 0, 7],
+    [0, 0, 0, 0, 7, 0, 9, 6, 0],
+    [0, 2, 6, 9, 0, 0, 1, 3, 0],
+    [0, 0, 0, 2, 9, 0, 3, 0, 4],
+    [9, 6, 0, 0, 0, 0, 0, 8, 2],
+    [5, 0, 2, 0, 4, 7, 0, 0, 0],
+    [0, 1, 3, 0, 0, 9, 8, 4, 0],
+    [0, 9, 7, 0, 2, 0, 0, 0, 0],
+    [6, 0, 0, 0, 0, 3, 0, 7, 0]
+];
