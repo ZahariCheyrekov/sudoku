dd713b17ed6acc10de31162959fa509dca2db691
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-05 17:43:48 +0300

    Update app.js

--- sudoku/app.js
+++ sudoku/app.js
@@ -22,170 +22,21 @@ const solvedPuzzle = [
     [6, 5, 8, 4, 1, 3, 2, 7, 9],
 ];
 
-let buttonSelected = null;
-let squareSelected = null;
+let buttonSelected;
+let squareSelected;
 let solved;
+let isPaused = false;
+let minutes = 0;
+let seconds = 0;
+let timerDiv;
 
-window.onload = function () {
-    run();
-};
+window.addEventListener('DOMContentLoaded', run);
 
 function run() {
     displayBoard();
     displayPuzzle();
     displayNumpad();
     attachListeners();
+    startTimer();
+    timerDiv = document.getElementById('sudoku-timer');
 }
-
-function displayBoard() {
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-
-            let square = document.createElement('div');
-            square.id = row.toString() + '-' + col.toString();
-            square.classList.add('squares');
-
-            if (row == 0) {
-                square.classList.add('border-top');
-            }
-            if (col == 0) {
-                square.classList.add('border-left');
-            }
-            if (row == 2 || row == 5 || row == 8) {
-                square.classList.add('border-bottom');
-            }
-            if (col == 2 || col == 5 || col == 8) {
-                square.classList.add('border-right');
-            }
-
-            square.addEventListener('click', selectSquare);
-            square.addEventListener('click', enterNumber);
-            document.getElementById('board').appendChild(square);
-        }
-    }
-}
-
-function displayPuzzle() {
-    solved = false;
-    const cells = getCells();
-
-    cells.forEach((cell) => {
-        const cellId = cell.id;
-        const coords = getCoordinates(cell);
-
-        if (puzzle[coords.row][coords.col] != 0) {
-            document.getElementById(cellId).textContent = puzzle[coords.row][coords.col];
-            cell.classList.add('displayPuzzleSquares');
-        } else {
-            document.getElementById(cellId).textContent = '';
-        }
-    });
-}
-
-function displayNumpad() {
-    for (let num = 1; num <= 10; num++) {
-        const button = document.createElement('button');
-
-        if (num == 10) {
-            button.textContent = 'X';
-            button.id = 'deleteBtn';
-        } else {
-            button.textContent = num;
-            button.id = `${num}-button`;
-        }
-
-        button.classList.add('numpadButtons');
-        button.addEventListener('click', selectNumber);
-        document.getElementById('numpad').appendChild(button);
-    }
-}
-
-function selectNumber() {
-    if (buttonSelected != null) {
-        buttonSelected.classList.remove('numSelected');
-    }
-    buttonSelected = this;
-    buttonSelected.classList.add('numSelected');
-}
-
-function selectSquare() {
-    if (squareSelected != this) {
-        if (squareSelected != null) {
-            squareSelected.classList.remove('cell-color');
-        }
-        squareSelected = this;
-    }
-    squareSelected.classList.add('cell-color');
-}
-
-function enterNumber() {
-    if (buttonSelected && !solved) {
-        const coords = getCoordinates(this);
-
-        if (puzzle[coords.row][coords.col] == 0) {
-            if (buttonSelected.textContent == 'X') {
-                this.innerText = '';
-                this.classList.remove('entered-numbers');
-            } else {
-                this.innerText = Number(buttonSelected.textContent);
-                this.classList.add('entered-numbers');
-            }
-        }
-    }
-}
-
-function getCoordinates(input) {
-    const coords = input.id.split('-');
-    return {
-        row: Number(coords[0]),
-        col: Number(coords[1])
-    }
-}
-
-function attachListeners() {
-    document.getElementsByClassName('game-action')[1].addEventListener('click', checkForErrors);
-    document.getElementsByClassName('game-action')[1].addEventListener('click', cehekSolution);
-    document.getElementsByClassName('game-action')[2].addEventListener('click', displayPuzzle);
-    document.getElementsByClassName('game-action')[3].addEventListener('click', solvePuzzle);
-}
-
-function solvePuzzle() {
-    const cells = getCells();
-
-    cells.forEach((cell) => {
-        const coords = getCoordinates(cell);
-        document.getElementById(cell.id).textContent = solvedPuzzle[coords.row][coords.col];
-        cell.classList.remove('entered-numbers');
-    });
-
-    solved = true;
-}
-
-function getCells() {
-    return document.querySelectorAll('.squares');
-}
-
-function cehekSolution() {
-    const cells = getCells();
-
-    for (const cell of cells) {
-        const coords = getCoordinates(cell);
-        const row = coords.row;
-        const col = coords.col;
-
-
-        if (document.getElementById(cell.id).textContent != solvedPuzzle[row][col]) {
-            alert('The solution is incorrect. Try again.');
-            return;
-        }
-    }
-    
-    solved = true;
-    alert('Congratulations! You solved the puzzle! :)');
-}
-
-function checkForErrors() {
-    //TODO: add implementation
-}
-
-//TODO: REFACTOR CODE!!! :)
